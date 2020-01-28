5c14297f61022abfa75fa4fb0a776a3ddb91dc5b
taroj khan <tarujkhan@yahoo.com>
2020-01-28 00:04:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -43,6 +43,6 @@ def now_serving(deli_line)
     puts "There is nobody waiting to be served!"
   else
     name = deli_line.shift
-    puts "Current serving #{name}"
+    puts "Current serving #{name}."
   end
 end
